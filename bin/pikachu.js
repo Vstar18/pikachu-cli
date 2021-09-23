697286dd00f1432fb44f75661cab22010a128abe
wei <weix13@ziroom.com>
2021-09-23 14:20:29 +0800

    feat:cli 完善

--- bin/pikachu.js
+++ bin/pikachu.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 const figlet = require('figlet');
 const Printer = require('@darkobits/lolcatjs');
-const versionStr = figlet.textSync('Hello Pikachu!')
+const versionStr = figlet.textSync('Yeah Pikachu!')
 const transformed = Printer.fromString(versionStr)
 const shell = require("shelljs");
 const inquirer = require('inquirer');
@@ -11,30 +11,53 @@ const download = require('download-git-repo');
 const chalk = require('chalk');
 const fs = require('fs');
 const {
-	writeJson
-} = require('./util');
-
+	writeJson,
+    writeJs
+} = require('./utils');
+const templateUrl = 'git@gitlab.ziroom.com:design-fe/cherry/cherry-template-vue3_antv_ts.git';
+const componentsTemplateUrl = 'git@gitlab.ziroom.com:design-fe/fe-template/react-typescript-activity-template.git';
 program.option('-c, --create<type>', '','');
 
 const dictionary = {
     create(env) {
         inquirer
         .prompt([
-        {
-            type:'text',
-            message:'请输入组件名称,示例pi-map',
-            name:'componentName'
-        }
+            {
+                type:'text',
+                message:'请输入组件名称,示例pi-map',
+                name:'componentName'
+            },
+            {
+                type:'list',
+                message:'请选择组件所属级别,示例：轻量级组件',
+                name:'componentLevel',
+                choices: [
+                    { name: '基础组件',value: 'pages/componentsA'},
+                    { name: '轻量级组件', value: 'pages/componentsB'},
+                    { name: '重量级组件', value: 'pages/componentsC' },
+                ]
+            },
+            {
+                type:'text',
+                message:'请输入组件标题名称,示例:按钮',
+                name:'componentBarTitle'
+            },
+            {
+                type:'text',
+                message:'请输入组件库演示用组件名称,示例:map',
+                name:'componentExampleName'
+            },
         ])
         .then(answers => {
             //创建pikachu目录下components下的组件文件
             const _pwd = shell.pwd().stdout;
+            console.log(_pwd,'_pwd===🍌')
             const projectPath = `${_pwd}/src/pikachu/components/${answers.componentName}`;
             const filename = `${projectPath}/template.vue`;
             const componentsName = `${projectPath}/${answers.componentName}.vue`;
             shell.rm('-rf',projectPath);
             shell.mkdir(projectPath);
-            const template = 'direct:git@github.com:Vstar18/pikachu-template.git';
+            const template = `direct:${templateUrl}`;
             download(template,projectPath,{clone:true},function (err) {
                 console.log(err,'err🍎')
                 if(err) {
@@ -53,12 +76,29 @@ const dictionary = {
                 })
             })
             //修改pages.json文件
-            let newJsonData = {
-                
-            }
-            writeJson(`${_pwd}/src/pages.json`)
+            writeJson(`${_pwd}/src/pages.json`,answers.componentLevel,{
+                "path": `${answers.componentName}/index`,
+                "style": {
+                    "navigationBarTitleText": `${answers.componentBarTitle}`
+                }
+            })
             //创建pages目录下的componentsA/B/C对应重量级的文件
-            //修改pages目录下的example下的componets.config.ts
+            const componentsPath = `${_pwd}/src/${answers.componentLevel}/${answers.componentExampleName}`;
+            shell.rm('-rf',componentsPath);
+            shell.mkdir(componentsPath);
+            const componentsTemplate = `direct:${componentsTemplateUrl}`;
+            download(componentsTemplate,componentsPath,{clone:true},function (err) {
+                if(err) {
+                    console.log(`${chalk.red('服务器出错了，下载失败！')}`)
+                }else{
+
+                }
+            })
+            //修改pages目录下的example下的components.config.ts
+            writeJs(`${_pwd}/src/pages/example/components.config.ts`,answers.componentLevel,{
+                "path": `/${answers.componentLevel}/${answers.componentExampleName}/index`,
+                "title": `${answers.componentBarTitle}`
+            })
         })
         .catch(error => {
             console.log(`${chalk.red(error)}`)
--- bin/utils.js
+++ bin/utils.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-const writeJson = (fileName,params)=> {
+const writeJson = (fileName,target,params)=> {
 
     fs.readFile(fileName,function(err,data){
         if(err){
@@ -9,9 +9,12 @@ const writeJson = (fileName,params)=> {
         let person = data.toString();//将二进制的数据转换为字符串
         person = JSON.parse(person);//将字符串转换为json对象
         console.log(person,'personjson对象🍎')
-        person.subPackages.push(params);//将传来的对象push进数组对象中
-        person.total = person.subPackages.length;//定义一下总条数，为以后的分页打基础
-        console.log(person.data);
+        let targetData = person.subPackages;
+        for(let key of targetData) {
+            if(key.root == target) {
+                key.pages.push(params)//将传来的对象push进数组对象中
+            }
+        }
         var str = JSON.stringify(person);//因为nodejs的写入文件只认识字符串或者二进制数，所以把json对象转换成字符串重新写入json文件中
         fs.writeFile(fileName,str,function(err){
             if(err){
@@ -22,6 +25,31 @@ const writeJson = (fileName,params)=> {
     })
 }
 
+const writeJs = (fileName,target,params) => {
+    fs.readFile(fileName,{ encoding:'utf-8'}, (err,data) => {
+        if(err) {
+            return console.error(err)
+        }
+        console.log(data,'datadata对象🍎🍎🍎🍎🍎')
+        let person = data.toString();//将二进制的数据转换为字符串
+        person = JSON.parse(person);//将字符串转换为json对象
+        console.log(person,'personjson对象🍎🍎🍎🍎🍎')
+        let targetData = person.list;
+        for(let i of targetData) {
+            if(key.value == target) {
+                key.list.push(params)//将传来的对象push进数组对象中
+            }
+        }
+        var str = JSON.stringify(person);//因为nodejs的写入文件只认识字符串或者二进制数，所以把json对象转换成字符串重新写入json文件中
+        fs.writeFile(fileName,str,function(err){
+            if(err){
+                console.error(err);
+            }
+            console.log('----------新增成功-------------');
+        })
+    })
+}
 module.exports = {
-    writeJson
+    writeJson,
+    writeJs
 }
